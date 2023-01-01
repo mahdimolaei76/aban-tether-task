5d2f24bed76fea9234a3d45eeb7be7f8a01bf033
m.mahdimolaei <mahdimolaei76@gmail.com>
2023-01-01 16:15:07 +0330

    init: initial files added for setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import RegisterAndLogin from "./pages/register/register"
+function App() {
+  return (
+    <div className="App">
+      <RegisterAndLogin />
+    </div>
+  );
+}
+
+export default App;
