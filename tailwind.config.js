f55c9c46c101a6f90d5fde323c024ac060db960b
m.molaei <mahdimolaei76@gmail.com>
2023-01-03 23:38:13 +0330

    fix: change config files for tailwind and app

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,11 @@ module.exports = {
   theme: {
     extend: {
       width:(theme) => ({
-        "container": "380px",
+        "container": "320px",
         ...theme("spacing"),
         "1/2": "50%",
       })
     },
   },
-  plugins: [
-    plugin(function ({ addComponents, theme }) {
-      addComponents({
-        '.will-change-trans': { // `will-change-transform` is now included in Tailwind CSS 3.0 so using alternative class name for this example
-          willChange: 'transform'
-        },
-        '.ease': {
-          transition: 'ease'
-        },
-      })
-    })
-  ]
+  plugins: []
 }
